2058ba01dfbad40fc401f0d8578221efbe7fe006
AndersKrog <70210262+AndersKrog@users.noreply.github.com>
2021-05-25 11:34:22 +0200

    Add files via upload

--- inputhandler.js
+++ inputhandler.js
@@ -11,11 +11,7 @@ export default class InputHandler{
 			if (event.keyCode == 40)		// DownArrow
 				visitor.walkDirection = 1;
 			if (event.keyCode == 86	)		// v
-				if (screen.viewMode == 1){
-					screen.viewMode = 0;
-				} else {
-					screen.viewMode = 1;
-				}
+				screen.shiftViewMode();
 			if (event.keyCode == 70	){		// f
 				visitor.shiftFloor();
 				screen.shiftFloor();
@@ -37,3 +33,25 @@ export default class InputHandler{
 		});	
 	}	
 }
+
+// pointer lock API
+//https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
+
+/*
+canvas.requestPointerLock = canvas.requestPointerLock ||
+                            canvas.mozRequestPointerLock;
+
+document.exitPointerLock = document.exitPointerLock ||
+                           document.mozExitPointerLock;
+
+canvas.onclick = function() {
+  canvas.requestPointerLock();
+  // skal eventuelt slås fra, hvis musen ikke er fanget
+
+	
+	//document.addEventListener("mousemove", function () {visitor.updatePosition(e,camera_FP)}, true);
+	//hvis man skal passe et argument sammen med eventet kan det gøres således:
+	
+	document.addEventListener("mousemove", (e) => {visitor.updatePosition(e,camera_FP)}, true);
+};
+*/
